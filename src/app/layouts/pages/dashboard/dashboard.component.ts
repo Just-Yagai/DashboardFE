ee188764be89a70b99d79eb882926b8210372a06
Sirius <romerperez26@gmail.com>
2023-05-23 22:53:04 -0400

    Funcion buscar por rnc y llenar campos automatico agregada

--- src/app/layouts/pages/dashboard/dashboard.component.ts
+++ src/app/layouts/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatosContribuyentesService } from 'src/app/services/datos-contribuyentes.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  rnc!: string;
+  datosContribuyente: any;
+  datosCertificacion: any;
+
+  constructor(private dataServices: DatosContribuyentesService){}
+
+  buscarContribuyente() {
+    
+    if (!this.rnc){
+      return;
+    }
+
+    this.dataServices.buscarContribuyentes(this.rnc).subscribe(
+      (response) => {
+        this.datosContribuyente = response.datos_contribuyentes[0];
+        this.datosCertificacion = response.datos_certificacion[0];
+      },
+      (error) => {
+        console.error('Error al buscar el contribuyente:', error);
+      }
+    );
+  }
 
 }
--- src/app/services/datos-contribuyentes.service.ts
+++ src/app/services/datos-contribuyentes.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatosContribuyentesService {
 
+  private apiUrl = 'assets/json/datos.json';
+
   constructor(private http: HttpClient) { }
+
+  buscarContribuyentes(rnc: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?rnc=${rnc}`);
+  }
 
   cargarDatos() {
 
